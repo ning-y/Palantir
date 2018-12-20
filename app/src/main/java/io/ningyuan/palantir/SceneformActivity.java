4a108591ac1cb20b2e5adbd5a56e3103bd7a6b85
ning <ningyuan.sg@gmail.com>
2018-12-20 18:22:59 +0800

    Implement import and rendering of glb files

--- app/src/main/java/io/ningyuan/palantir/SceneformActivity.java
+++ app/src/main/java/io/ningyuan/palantir/SceneformActivity.java
@@ -4,31 +4,34 @@ import android.app.Activity;
 import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Build.VERSION_CODES;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.MotionEvent;
-import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 import com.google.ar.core.Anchor;
 import com.google.ar.core.HitResult;
 import com.google.ar.core.Plane;
 import com.google.ar.sceneform.AnchorNode;
 import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.assets.RenderableSource;
 import com.google.ar.sceneform.ux.ArFragment;
 import com.google.ar.sceneform.ux.TransformableNode;
+import org.apache.commons.io.IOUtils;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class SceneformActivity extends AppCompatActivity {
   private static final String TAG = SceneformActivity.class.getSimpleName();
   private static final double MIN_OPENGL_VERSION = 3.0;
   private static final float SCALE_HACK_MAX = 0.1f;   // TODO: make this less hacky
   private static final float SCALE_HACK_MIN = 0.05f;  // TODO: make this less hacky
-  private static final int IMPORT_GLTF_FILE_RESULT = 1;
+  private static final int IMPORT_GLB_FILE_RESULT = 1;
 
   private ArFragment arFragment;
   private ModelRenderable modelRenderable;
@@ -46,18 +49,7 @@ public class SceneformActivity extends AppCompatActivity {
 
     setContentView(R.layout.activity_ux);
 
-    // When you build a Renderable, Sceneform loads its resources in the background while returning
-    // a CompletableFuture. Call thenAccept(), handle(), or check isDone() before calling get().
-    ModelRenderable.builder()
-        .setSource(this, R.raw.cro)
-        .build()
-        .thenAccept(renderable -> modelRenderable = renderable)
-        .exceptionally(
-            throwable -> {
-              Toast.makeText(this, "Unable to load renderable", Toast.LENGTH_LONG).show();
-              return null;
-            });
-
+    setModelRenderable(R.raw.cro);
     arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
     arFragment.setOnTapArPlaneListener(
         (HitResult hitResult, Plane plane, MotionEvent motionEvent) -> {
@@ -83,18 +75,88 @@ public class SceneformActivity extends AppCompatActivity {
         });
 
     final FloatingActionButton importButton = findViewById(R.id.import_button);
-    importButton.setOnClickListener((View v) -> {
+    importButton.setOnClickListener(view -> {
         Intent importIntent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
-        importIntent.setType("*/*");  // otherwise, there will be no resolvable activity
+        importIntent.addCategory(Intent.CATEGORY_OPENABLE);
+        importIntent.setType("*/*");  // MIME type for glTF not yet supported; https://issuetracker.google.com/issues/121223582
 
         // Only startActivity if there is a resolvable activity; if not checked, will crash
         if (importIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(importIntent, IMPORT_GLTF_FILE_RESULT);
+            startActivityForResult(importIntent, IMPORT_GLB_FILE_RESULT);
         } else {
-            Toast.makeText(getApplicationContext(), "Something went wrong: no resolvable activity for importIntent.", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Something went wrong: no resolvable activity for importIntent.", Toast.LENGTH_LONG).show();
         }
     });
   }
+
+  /**
+   * Receive a result from the importButton's importIntent i.e. Intent.ACTION_OPEN_DOCUMENT
+   * @param requestCode the request code passed to startActivityForResult i.e. IMPORT_GLB_FILE_RESULT
+   * @param resultCode 'exit code' by the external activity; RESULT_OK or RESULT_CANCELED
+   * @param resultIntent an Intent carrying data
+   */
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent resultIntent) {
+    if (requestCode == IMPORT_GLB_FILE_RESULT && resultCode == RESULT_OK) {
+        Uri uri = resultIntent.getData();
+        setModelRenderable(uri);
+    }
+  }
+
+  private void setModelRenderable(int resource) {
+      ModelRenderable.builder()
+          .setSource(this, resource)
+          .build()
+          .thenAccept(renderable -> modelRenderable = renderable)
+          .exceptionally(
+              throwable -> {
+                  Toast.makeText(this, "Unable to load andy renderable", Toast.LENGTH_LONG).show();
+                  return null;
+              });
+  }
+
+  /**
+   * @param uri content URI returned from ACTION_OPEN_DOCUMENT's resulting intent
+   *
+   * So, the URI that is passed here is a content URI from the ACTION_OPEN_DOCUMENT intent.
+   * However, RenderableSource.builder().setSource really only accepts URLs, not URIs (this
+   * restriction is not documented). So, take the content URI, open an inputStream, save it
+   * in this app's cache directory, then pass that new file in the cache to
+   * ReadnerableSource.builder().setSource.
+   */
+  private void setModelRenderable(Uri uri) {
+      try {
+          Log.e("setModelRenderable", "Start writing file");
+          String cacheFileName = String.valueOf(System.currentTimeMillis()); // uri.getLastPathSegment();
+          File cacheFile = File.createTempFile(cacheFileName, ".glb", getCacheDir());
+          FileOutputStream outputStream = new FileOutputStream(cacheFile);
+          IOUtils.copy(getContentResolver().openInputStream(uri), outputStream);
+          outputStream.close();
+          Log.e("setModelRenderable", "Done writing file " + cacheFile.getCanonicalPath());
+
+          ModelRenderable.builder()
+                  .setSource(this, RenderableSource.builder().setSource(
+                          this,
+                          Uri.parse(cacheFile.toURI().toString()),
+                          RenderableSource.SourceType.GLB)
+                          .setScale(0.5f)
+                          .setRecenterMode(RenderableSource.RecenterMode.ROOT)
+                          .build()
+                  )
+                  .setRegistryId(cacheFileName)
+                  .build()
+                  .thenAccept(renderable -> modelRenderable = renderable)
+                  .exceptionally(
+                          throwable -> {
+                              Log.e("setModelRenderable", null, throwable);
+                              Toast.makeText(this, "Unable to load renderable", Toast.LENGTH_LONG).show();
+                              return null;
+                          });
+      } catch (IOException e) {
+          Log.e("setModelRenderable", "Failed with IOException: " + e.toString());
+          Toast.makeText(this, "Unable to import file.", Toast.LENGTH_LONG).show();
+      }
+}
 
   /**
    * Returns false and displays an error message if Sceneform can not run, true if Sceneform can run
