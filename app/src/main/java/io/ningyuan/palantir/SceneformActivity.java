4bcb080011580288bca10a1368a3fd3beb406c9b
ning <ningyuan.sg@gmail.com>
2019-01-07 19:34:55 +0800

    Abstract AR logic to its own class

--- app/src/main/java/io/ningyuan/palantir/SceneformActivity.java
+++ app/src/main/java/io/ningyuan/palantir/SceneformActivity.java
@@ -1,30 +1,17 @@
 package io.ningyuan.palantir;
 
-import android.app.Activity;
-import android.app.ActivityManager;
-import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.MotionEvent;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.google.ar.core.Anchor;
-import com.google.ar.core.HitResult;
-import com.google.ar.core.Plane;
-import com.google.ar.sceneform.AnchorNode;
-import com.google.ar.sceneform.assets.RenderableSource;
-import com.google.ar.sceneform.rendering.ModelRenderable;
-import com.google.ar.sceneform.ux.ArFragment;
-import com.google.ar.sceneform.ux.BaseArFragment;
-import com.google.ar.sceneform.ux.TransformableNode;
 
 import java.io.File;
 import java.io.IOException;
 
+import io.ningyuan.palantir.fragments.SceneformFragment;
 import io.ningyuan.palantir.views.ImportButton;
 
 import static io.ningyuan.palantir.utils.FileIo.cacheFileFromContentUri;
@@ -34,14 +21,9 @@ import static io.ningyuan.palantir.views.ImportButton.IMPORT_MODE_GLB;
 import static io.ningyuan.palantir.views.ImportButton.IMPORT_MODE_OBJ;
 
 public class SceneformActivity extends AppCompatActivity {
-    private static final String TAG = SceneformActivity.class.getSimpleName();
-    private static final double MIN_OPENGL_VERSION = 3.0;
-    private static final float SCALE_HACK_MAX = 0.1f;   // TODO: make this less hacky
-    private static final float SCALE_HACK_MIN = 0.05f;  // TODO: make this less hacky
+    public static final String TAG = SceneformActivity.class.getSimpleName();
 
-    private ArFragment arFragment;
-    private ModelRenderable modelRenderable;
-    private String modelName;
+    private SceneformFragment sceneformFragment;
     private TextView modelNameTextView;
     private int importMode;
 
@@ -52,22 +34,22 @@ public class SceneformActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (!checkIsSupportedDeviceOrFinish(this)) {
+        if (!SceneformFragment.checkIsSupportedDeviceOrFinish(this)) {
             return;
         }
 
         setContentView(R.layout.activity_ux);
 
         modelNameTextView = findViewById(R.id.model_name);
-        updateModelNameTextView();
+        updateModelNameTextView("First, import a 3D model from glb or obj");
 
         final ImportButton importGlbButton = findViewById(R.id.import_glb_button);
         importGlbButton.setImportModeToTrigger(IMPORT_MODE_GLB);
         final ImportButton importObjButton = findViewById(R.id.import_obj_button);
         importObjButton.setImportModeToTrigger(IMPORT_MODE_OBJ);
 
-        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
-        arFragment.setOnTapArPlaneListener(new ArFragmentTapListener());
+        sceneformFragment = (SceneformFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
+        sceneformFragment.setParentActivity(this);
     }
 
     public void setImportMode(int importMode) {
@@ -94,7 +76,7 @@ public class SceneformActivity extends AppCompatActivity {
                         Uri contentUri = resultIntent.getData();
                         String filename = getFilenameFromContentUri(this, contentUri);
                         File cacheFile = cacheFileFromContentUri(this, contentUri, ".glb");
-                        setModelRenderable(filename, cacheFile);
+                        sceneformFragment.setModelRenderable(filename, cacheFile);
                     } catch (IOException e) {
                         Log.e(TAG, getString(R.string.log_import_failed_io) + e.toString());
                         showToast(R.string.error_import_failed_io, Toast.LENGTH_LONG);
@@ -109,38 +91,9 @@ public class SceneformActivity extends AppCompatActivity {
     /**
      * Update modelNameTextView to show the current value of modelName.
      */
-    private void updateModelNameTextView() {
+    public void updateModelNameTextView(String newModelName) {
         // Using String.valueOf accounts for when modelName == null
-        modelNameTextView.setText(String.valueOf(modelName));
-    }
-
-    /**
-     * Set a binary glTF file (*.glb) as the modelRenderable.
-     */
-    private void setModelRenderable(String filename, File cacheFile) {
-        // Weird URI conversions coming up because java.net.URI != android.net.URI
-        ModelRenderable.builder()
-                .setSource(this, RenderableSource.builder().setSource(
-                        this,
-                        Uri.parse(cacheFile.toURI().toString()),
-                        RenderableSource.SourceType.GLB)
-                        .setScale(0.5f)
-                        .setRecenterMode(RenderableSource.RecenterMode.ROOT)
-                        .build()
-                )
-                .setRegistryId(cacheFile.getName())
-                .build()
-                .thenAccept(renderable -> {
-                    modelRenderable = renderable;
-                    modelName = filename;
-                    updateModelNameTextView();
-                })
-                .exceptionally(
-                        throwable -> {
-                            Log.e(TAG, null, throwable);
-                            showToast(R.string.error_import_failed_bad_render, Toast.LENGTH_LONG);
-                            return null;
-                        });
+        modelNameTextView.setText(newModelName);
     }
 
     /**
@@ -161,57 +114,5 @@ public class SceneformActivity extends AppCompatActivity {
      */
     private void showToast(final int resID, final int duration) {
         showToast(getString(resID), duration);
-    }
-
-    /**
-     * Returns false and displays an error message if Sceneform can not run, true if Sceneform can run
-     * on this device.
-     *
-     * <p>Sceneform requires Android N on the device as well as OpenGL 3.0 capabilities.
-     *
-     * <p>Finishes the activity if Sceneform can not run
-     */
-    public boolean checkIsSupportedDeviceOrFinish(final Activity activity) {
-        String openGlVersionString =
-                ((ActivityManager) activity.getSystemService(Context.ACTIVITY_SERVICE))
-                        .getDeviceConfigurationInfo()
-                        .getGlEsVersion();
-        if (Double.parseDouble(openGlVersionString) < MIN_OPENGL_VERSION) {
-            Log.e(TAG, getString(R.string.error_insufficient_opengl_version));
-            showToast(R.string.error_insufficient_opengl_version, Toast.LENGTH_LONG);
-            activity.finish();
-            return false;
-        }
-        return true;
-    }
-
-    /**
-     * OnTapArPlaneListener for arFragment.
-     */
-    private class ArFragmentTapListener implements BaseArFragment.OnTapArPlaneListener {
-        @Override
-        public void onTapPlane(HitResult hitResult, Plane plane, MotionEvent motionEvent) {
-            if (modelRenderable == null) {
-                showToast(R.string.error_no_model_renderable, Toast.LENGTH_SHORT);
-            }
-
-            // Create the Anchor.
-            Anchor anchor = hitResult.createAnchor();
-            AnchorNode anchorNode = new AnchorNode(anchor);
-            anchorNode.setParent(arFragment.getArSceneView().getScene());
-
-            // Create the transformable node and add it to the anchor.
-            TransformableNode transformableNode =
-                    new TransformableNode(arFragment.getTransformationSystem());
-            transformableNode.setParent(anchorNode);
-            transformableNode.setRenderable(modelRenderable);
-            transformableNode.select();
-
-            // The model normally spawns way too large. As a work-around (i.e. hack),
-            // bound it's scale values to the ones where it looks reasonably large.
-            // TODO: make it less hacky, of course
-            transformableNode.getScaleController().setMaxScale(SCALE_HACK_MAX);
-            transformableNode.getScaleController().setMinScale(SCALE_HACK_MIN);
-        }
     }
 }
---
+++ app/src/main/java/io/ningyuan/palantir/fragments/SceneformFragment.java
@@ -0,0 +1,127 @@
+package io.ningyuan.palantir.fragments;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.ar.core.Anchor;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.Plane;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.assets.RenderableSource;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.BaseArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
+
+import java.io.File;
+
+import io.ningyuan.palantir.R;
+import io.ningyuan.palantir.SceneformActivity;
+
+public class SceneformFragment extends ArFragment {
+    private static final double MIN_OPENGL_VERSION = 3.0;
+    private static final float SCALE_HACK_MAX = 0.1f;   // TODO: make this less hacky
+    private static final float SCALE_HACK_MIN = 0.05f;  // TODO: make this less hacky
+
+    private SceneformActivity parentActivity;
+    private ModelRenderable modelRenderable;
+
+    /**
+     * Returns false and displays an error message if Sceneform can not run, true if Sceneform can run
+     * on this device.
+     *
+     * <p>Sceneform requires Android N on the device as well as OpenGL 3.0 capabilities.
+     *
+     * <p>Finishes the activity if Sceneform can not run
+     */
+    public static boolean checkIsSupportedDeviceOrFinish(final Activity activity) {
+        String openGlVersionString =
+                ((ActivityManager) activity.getSystemService(Context.ACTIVITY_SERVICE))
+                        .getDeviceConfigurationInfo()
+                        .getGlEsVersion();
+        if (Double.parseDouble(openGlVersionString) < MIN_OPENGL_VERSION) {
+            Log.e(SceneformActivity.TAG, activity.getString(R.string.error_insufficient_opengl_version));
+            Toast toast = Toast.makeText(activity, R.string.error_insufficient_opengl_version, Toast.LENGTH_LONG);
+            activity.finish();
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View inflatedView = super.onCreateView(inflater, container, savedInstanceState);
+        this.setOnTapArPlaneListener(new ArFragmentTapListener());
+        return inflatedView;
+    }
+
+    public void setParentActivity(SceneformActivity activity) {
+        this.parentActivity = activity;
+    }
+
+    /**
+     * Set a binary glTF file (*.glb) as the modelRenderable.
+     */
+    public void setModelRenderable(String filename, File cacheFile) {
+        // Weird URI conversions coming up because java.net.URI != android.net.URI
+        ModelRenderable.builder()
+                .setSource(parentActivity, RenderableSource.builder().setSource(
+                        parentActivity,
+                        Uri.parse(cacheFile.toURI().toString()),
+                        RenderableSource.SourceType.GLB)
+                        .setScale(0.5f)
+                        .setRecenterMode(RenderableSource.RecenterMode.ROOT)
+                        .build()
+                )
+                .setRegistryId(cacheFile.getName())
+                .build()
+                .thenAccept(renderable -> {
+                    modelRenderable = renderable;
+                    parentActivity.updateModelNameTextView(filename);
+                })
+                .exceptionally(
+                        throwable -> {
+                            Log.e(SceneformActivity.TAG, null, throwable);
+                            Toast toast = Toast.makeText(parentActivity, R.string.error_import_failed_bad_render, Toast.LENGTH_LONG);
+                            toast.show();
+                            return null;
+                        });
+    }
+
+    private class ArFragmentTapListener implements BaseArFragment.OnTapArPlaneListener {
+        @Override
+        public void onTapPlane(HitResult hitResult, Plane plane, MotionEvent motionEvent) {
+            if (modelRenderable == null) {
+                Toast toast = Toast.makeText(parentActivity, R.string.error_no_model_renderable, Toast.LENGTH_LONG);
+                toast.show();
+            }
+
+            // Create the Anchor.
+            Anchor anchor = hitResult.createAnchor();
+            AnchorNode anchorNode = new AnchorNode(anchor);
+            anchorNode.setParent(getArSceneView().getScene());
+
+            // Create the transformable node and add it to the anchor.
+            TransformableNode transformableNode = new TransformableNode(getTransformationSystem());
+            transformableNode.setParent(anchorNode);
+            transformableNode.setRenderable(modelRenderable);
+            transformableNode.select();
+
+            // The model normally spawns way too large. As a work-around (i.e. hack),
+            // bound it's scale values to the ones where it looks reasonably large.
+            // TODO: make it less hacky, of course
+            transformableNode.getScaleController().setMaxScale(SCALE_HACK_MAX);
+            transformableNode.getScaleController().setMinScale(SCALE_HACK_MIN);
+        }
+    }
+
+}
