9c25ceb53c0e582702ca035a2c38476145223924
ning <ningyuan.sg@gmail.com>
2019-08-12 19:01:24 +0800

    Fix progressBar not showing

--- app/src/main/java/io/ningyuan/palantir/utils/PdbSearcher.java
+++ app/src/main/java/io/ningyuan/palantir/utils/PdbSearcher.java
@@ -37,6 +37,7 @@ public class PdbSearcher extends AsyncTask<String, Void, MatrixCursor> {
 
     @Override
     protected void onPreExecute() {
+        Log.d(TAG, "onPreExecute called.");
         if (progressBar != null) {
             progressBar.setVisibility(View.VISIBLE);
         }
@@ -52,21 +53,21 @@ public class PdbSearcher extends AsyncTask<String, Void, MatrixCursor> {
             List<String> results = query.execute();
             MatrixCursor cursor = SearchView.getEmptyCursor(false);
             int index = 1;
-            Log.i(TAG, "Start for in doInBackground");
+            Log.i(TAG, String.format("Start doInBackground for %s", queryString));
             for (String pdbId : results) {
-                Log.i(TAG, pdbId);
+                Log.d(TAG, String.format("Fetching %s for %s", pdbId, queryString));
                 Pdb pdb = new Pdb(pdbId);
                 try {
                     pdb.load();
                     cursor.addRow(new Object[]{index++, pdb.getStructureId(), pdb.getTitle()});
                 } catch (FileNotFoundException e) {
-                    Log.e(TAG, "Encountered an exception.", e);
+                    Log.e(TAG, String.format("Encountered an exception for %s.", queryString), e);
                 }
             }
 
             return cursor;
         } catch (IOException | ParserConfigurationException e) {
-            Log.e(TAG, null, e);
+            Log.e(TAG, String.format("Encountered an exception for %s.", queryString), e);
             return null;
         }
     }
@@ -92,9 +93,14 @@ public class PdbSearcher extends AsyncTask<String, Void, MatrixCursor> {
 
     @Override
     protected void onCancelled(MatrixCursor cursor) {
+        // On cancelled, do nothing. I make this explicit because method used to set the progressBar
+        // visibility to View.INVISIBLE. That was not a good idea: when a search was cancelled and
+        // another started in its place immediately, a race condition was created. One would expect
+        // onCancelled to be called first, setting the progressBar to invisible; then, onPreExecute
+        // of the new pdbSearch would set progressBar to View.VISIBLE. In reality, onPreExecute is
+        // often called before onCancelled, so the progressBar would not show. Instead, I set the
+        // progressBar visibility to View.INVISIBLE explicitly in the SearchView methods.
+        Log.d(TAG, "onCancelled called.");
         inProgress = false;
-        if (progressBar != null) {
-            progressBar.setVisibility(View.INVISIBLE);
-        }
     }
 }
